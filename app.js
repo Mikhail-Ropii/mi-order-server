ade301e40851f12027aae073471da6658151e0bb
Mikhail-Ropii <mr.ropiy@gmail.com>
2023-01-17 22:16:52 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const sendOrder = require("./helpers/sendOrder");
+
+dotenv.config();
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.post("/sendOrder", (req, res) => {
+  sendOrder(req.body);
+  res.status(200).json({ message: "Order sended" });
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server Error" } = err;
+  res.status(status).json({ message });
+});
+
+module.exports = app;
---
+++ helpers/sendOrder.js
@@ -0,0 +1,46 @@
+const fs = require("fs/promises");
+const nodemailer = require("nodemailer");
+const path = require("path");
+const XLSX = require("xlsx");
+require("dotenv").config();
+
+const { MAIL_PASS } = process.env;
+
+const orderPath = path.join(__dirname, "../temp");
+
+const sendOrder = async ({ items, clientName, managerName }) => {
+  const convertToXlsx = async () => {
+    const workSheet = XLSX.utils.json_to_sheet(items);
+    const workBook = XLSX.utils.book_new();
+
+    XLSX.utils.book_append_sheet(workBook, workSheet, `${clientName}`);
+    XLSX.write(workBook, { bookType: "xlsx", type: "buffer" });
+    XLSX.write(workBook, { bookType: "xlsx", type: "binary" });
+    XLSX.writeFile(workBook, `${orderPath}/${clientName}.xlsx`);
+  };
+  await convertToXlsx();
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ukr.net",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "mega_sendgrid@ukr.net",
+      pass: MAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: "mega_sendgrid@ukr.net",
+    to: "mr.ropiy@gmail.com",
+    subject: `${managerName}`,
+    text: `Замовлення від ${managerName}, клієнт ${clientName}`,
+    attachments: [{ path: `${orderPath}/${clientName}.xlsx` }],
+  };
+
+  await transporter.sendMail(mailOptions);
+
+  // Delete temp file
+  fs.unlink(`${orderPath}/${clientName}.xlsx`);
+};
+
+module.exports = sendOrder;
